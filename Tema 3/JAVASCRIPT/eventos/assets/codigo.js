709dce86720462af1bbdc178ebaddf47600a1e62
MiriamJimenezIesBelen <mjimsan1710@g.educaand.es_MiriamJimenezIesBelen>
2025-02-05 13:40:07 +0100

    eventos

--- Tema 3/JAVASCRIPT/eventos/assets/codigo.js
+++ Tema 3/JAVASCRIPT/eventos/assets/codigo.js
@@ -1,12 +1,12 @@
 
 const p1=document.querySelector(".niidea");
-const b=document.querySelector(".b");
+const b=document.querySelector("body");
 const p2=document.querySelector("#2");
 
 p1.addEventListener("click",()=>alert("Parrafo 1 click"));
 b.addEventListener("load",mensaje);
 p2.addEventListener("dblclick",() => alert("Parrafo 2 doble click"));
 
-function mensaje(){
+function mensaje {
     alert("Pagina cargada");
 }
