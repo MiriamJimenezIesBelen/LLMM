2246ed513000a8cada2d673411df8c34996289fa
MiriamJimenezIesBelen <mjimsan1710@g.educaand.es_MiriamJimenezIesBelen>
2025-02-12 12:02:09 +0100

    p3

--- Tema 3/JAVASCRIPT/eventos/p3/assets/js/codigo.js
+++ Tema 3/JAVASCRIPT/eventos/p3/assets/js/codigo.js
@@ -1,42 +1,34 @@
 
+const b = document.querySelector("body");
+const p1 = document.querySelector("#unclik");
+const foto1 = document.querySelector("#fotoclik");
+const foto2 = document.querySelector("#fotocursor");
+const p2 = document.querySelector("#dobleclik");
 
-const b=document.querySelector("body");
-const p1=document.querySelector("#unclik");
-const foto1=document.querySelector("#fotoclik");
-const foto2=document.querySelector("#fotocursor");
-const p2=document.querySelector("#dobleclik");
+// Evento cuando la web se carga completamente
+window.addEventListener("load", mensaje);
 
-b.addEventListener("onunload",mensaje);
-p1.addEventListener("onclick",()=>alert("Has realizado un click"));
-foto1.addEventListener("onclick",maximo);
-p2.addEventListener("ondblclick",() => alert("Parrafo 2 doble click"));
+// Evento para clic en el primer párrafo
+p1.addEventListener("click", () => alert("Has realizado un click"));
 
-function mensaje (){
+// Evento para clic en la imagen del robot
+foto1.addEventListener("click", maximo);
+
+// Evento para doble clic en el segundo párrafo
+p2.addEventListener("dblclick", () => alert("Parrafo 2 doble click"));
+
+function mensaje() {
     alert("Bienvenidos a la práctica de eventos");
 }
 
-function maximo(){
+function maximo() {
     const num1 = parseInt(prompt("Introduce un número: "));
     const num2 = parseInt(prompt("Introduce otro número: "));
 
-    if(isNaN(num1) && isNaN(num2)){
-        var maximo = num1>num2? num1:num2; 
-    }else{
-        alert("El número no es valido");
+    if (isNaN(num1) || isNaN(num2)) {
+        alert("El número no es válido");
+    } else {
+        const maximo = num1 > num2 ? num1 : num2;
+        alert(`El número máximo es ${maximo}`);
     }
-
-    alert("El número máximo es %d"+maximo);
 }
-
-
-
-
-
-
-
-
-//const num = parseInt(promport ("intrdouce numero"));
-
-//if isNaN(num){
- //   "es valido"
-//}
