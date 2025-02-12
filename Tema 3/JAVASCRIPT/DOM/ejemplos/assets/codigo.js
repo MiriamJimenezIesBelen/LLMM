9af31db52f1a20a36614b701f8fce67030f9261b
MiriamJimenezIesBelen <mjimsan1710@g.educaand.es_MiriamJimenezIesBelen>
2025-02-12 13:42:38 +0100

    ejemplos

---
+++ Tema 3/JAVASCRIPT/DOM/ejemplos/assets/codigo.js
@@ -0,0 +1,16 @@
+
+const enc = document.querySelector("#enc");
+const p = document.querySelector(".p1");
+const img = document.querySelector(".img");
+const b1 = document.querySelector(".b1");
+const b2 = document.querySelector(".b2");
+const b3 = document.querySelector(".b3");
+
+
+b1.addEventListener("click",() => {p.style.color="red";});
+
+b2.addEventListener("click",() => {img.src="./assets/manzana.png";});
+
+//b3.addEventListener("click",() => {enc.classList.toggle("nueva");});
+
+console.log(enc); // Esto debería mostrar el elemento <h1> en la consola
