c981c5ade831a98e08c48cfa5e05cafc12c95a66
MiriamJimenezIesBelen <mjimsan1710@g.educaand.es_MiriamJimenezIesBelen>
2025-02-13 12:44:10 +0100

    p4

---
+++ Tema 3/JAVASCRIPT/DOM/p4/assets/js/codigo.js
@@ -0,0 +1,13 @@
+
+const p1 = document.querySelector(".p1");
+
+
+
+const p = document.querySelector(".p1");
+const img = document.querySelector(".img");
+const b1 = document.querySelector(".b1");
+const b2 = document.querySelector(".b2");
+const b3 = document.querySelector(".b3");
+
+
+//p1.addEventListener("onmouseover",() => {p.style.color="red";});
