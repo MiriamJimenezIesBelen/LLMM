19b94e8daac564f47a7e4ce73cc136c783a5a820
MiriamJimenezIesBelen <mjimsan1710@g.educaand.es_MiriamJimenezIesBelen>
2025-02-17 09:14:59 +0100

    p4

--- Tema 3/JAVASCRIPT/DOM/p4/assets/js/codigo.js
+++ Tema 3/JAVASCRIPT/DOM/p4/assets/js/codigo.js
@@ -1,13 +1,45 @@
 
-const p1 = document.querySelector(".p1");
 
-
-
-const p = document.querySelector(".p1");
+let p1 = document.querySelector(".p1");
 const img = document.querySelector(".img");
-const b1 = document.querySelector(".b1");
-const b2 = document.querySelector(".b2");
-const b3 = document.querySelector(".b3");
+const botonFondo = document.querySelector(".cambiarFondo");
 
 
-//p1.addEventListener("onmouseover",() => {p.style.color="red";});
+p1.addEventListener("mouseover", () => { p1.style.color = "red";});
+p1.addEventListener("mouseout", () => { p1.style.color = "black";});
+
+img.addEventListener("mouseover", () => {img.src="./assets/invisible.jpg";})
+img.addEventListener("mouseout", () => {img.src="./assets/robot.jpg";})
+
+botonFondo.addEventListener("click",cambiarFondo);
+
+// no sale nada
+function edad() {
+    const edad = document.querySelector(".edad");
+    let result = parseInt(edad.value);
+    const borde = document.querySelector(".formulario");
+    const mensaje = document.querySelector(".mensaje");
+
+    if (isNaN(result)) {
+        mensaje.textContent = "Introduce un número válido";
+        mensaje.style.color = "red";
+        borde.style.borderColor = "red";
+
+    } else if (result >= 1 && result <= 100) {
+        mensaje.textContent = "Número correcto";
+        mensaje.style.color = "green";
+        borde.style.borderColor = "green";
+    } else {
+        mensaje.textContent = "Introduce un valor entre 1 y 100";
+        mensaje.style.color = "red";
+        borde.style.borderColor = "red";
+    }
+}
+
+
+function cambiarFondo(){
+    const cajasNegras = document.querySelector(".cajasNegras");
+    cajasNegras.forEach(cajasNegras => {
+        cajasNegras.classList.toggle("rojo");
+    });
+}
