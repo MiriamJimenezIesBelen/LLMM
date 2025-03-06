6a8abd637e7d4ea9695c10cb801669ae9151343f
MiriamJimenezIesBelen <mjimsan1710@g.educaand.es_MiriamJimenezIesBelen>
2025-03-06 12:13:51 +0100

    ejer 6.1

--- Tema 3/JAVASCRIPT/JSON/ejercicio 6.1/assets/codigo.js
+++ Tema 3/JAVASCRIPT/JSON/ejercicio 6.1/assets/codigo.js
@@ -12,19 +12,27 @@ async function buscarPelicula(){
     const titulo = document.querySelector(".titulo");
     const anyo = document.querySelector(".anyo");
     const duracion = document.querySelector(".duracion");
+    const calificacion = document.querySelector(".calificacion");
 
     try{
         const response = await fetch(url);
         const data = await response.json();
 
-        titulo.innerHTML = `<h3>${data.titulo}</h3>`;
-        anyo.innerHTML = `<h3>${data.anyo}</h3>`;
-        duracion.innerHTML = `<h3>${data.duracion}</h3>`;
+        titulo.innerHTML = `<h3>${data.Title}</h3>`;
+        anyo.innerHTML = `<h3>${data.Year}</h3>`;
+        duracion.innerHTML = `<h3>${data.Runtime}</h3>`;
+
+        calificacion.innerHTML = "";
+
+        data.Ratings.forEach((rating) => {
+            calificacion.innerHTML  += `<ul><li>${rating.Source}: ${rating.Value}</li></ul>`;
+        });
 
     }catch(error){
         titulo.innerHTML = "<p>No se encontró la película";
         anyo.innerHTML = "";
         duracion.innerHTML = "";
+        calificacion.innerHTML = "";
         console.error("Error al obtener datos",error);
     }
 }
